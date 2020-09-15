6c6ce3ca12df66bebb14f79eea8ee21f18b71673
Johan Onsjö <johan.onsjo@gmail.com>
2020-09-15 20:55:33 +0200

    extracted helper method for email creation and hard coded values from property file

--- src/main/java/com/effcode/clean/me/rest/handler/EmailHandler.java
+++ src/main/java/com/effcode/clean/me/rest/handler/EmailHandler.java
@@ -3,6 +3,7 @@ package com.effcode.clean.me.rest.handler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.effcode.clean.me.support.SmtpEmail;
@@ -14,6 +15,13 @@ public class EmailHandler {
     private static final Logger LOG = LoggerFactory.getLogger(EmailHandler.class);
 
     private final SmtpHandler smtpHandler;
+
+
+    @Value("app.email.username")
+    private String emailUsername;
+
+    @Value("app.email.password")
+    private String emailPassword;
 
 
     public EmailHandler(SmtpHandler smtpHandler) {
@@ -35,17 +43,24 @@ public class EmailHandler {
             return false;
         }
 
-        SmtpEmail smtpEmail = new SmtpEmail();
-        smtpEmail.adrs = new String[] {adr};
-        smtpEmail.content = content;
-        smtpEmail.subject = subject;
-        smtpEmail.password =  "secret";
-        smtpEmail.username = "foo";
+        SmtpEmail smtpEmail = createEmail(adr, subject, content);
         smtpHandler.post(smtpEmail);
 
         LOG.info("Send email. Adr: {}, Subject: {}", adr, subject);
 
         return true;
-    } 
+    }
+
+
+    private SmtpEmail createEmail(String adr, String subject, String content) {
+        SmtpEmail smtpEmail = new SmtpEmail();
+        smtpEmail.adrs = new String[] {adr};
+        smtpEmail.content = content;
+        smtpEmail.subject = subject;
+        smtpEmail.username = emailUsername;
+        smtpEmail.password =  emailPassword;
+        return smtpEmail;
+    }
+
 
 }
